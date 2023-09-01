9d7c5867e1cb26afecbb298f4981d17244a7c885
Vargthon Nunes <vargthon@gmail.com>
2023-09-01 08:35:21 -0300

    Adicionado usuário costumizado, apenas isso para iniciar o projeto por issues

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Custom User Manager class
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValidationError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User model class
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
